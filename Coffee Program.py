bf6de873a13431193a8f6627d2336925b91a1911
Isaac-Bandara <isaac.bandara@kingsway.school.nz>
2025-10-24 10:09:36 +1300

    Version 3.1 Coffee Program

--- Coffee Program.py
+++ Coffee Program.py
@@ -37,29 +37,32 @@ while keep_going == "":
 
 # Version 3
 # Making the program more pythonic.
-
-keep_going = ""
-while keep_going == "":
-    # Convert answer to lowercase using .lower()
-    like_coffee = input("Do you like coffee?\n").lower()
-    
-    if like_coffee == "yes" or like_coffee == "y":
-        print("That's great! I like coffee too!")
-    
-    elif like_coffee == "no" or like_coffee == "n":
-        print("You're missing out! Why not give it a try?")
-
-        like_tea = input("Do you like tea instead\n").upper()
-        if like_tea == "YES" or like_tea == "Y":
-            print("Good for you. Give coffee another try. :)")
+def coffee_program():
+    keep_going = ""
+    while keep_going == "":
+        # Convert answer to lowercase using .lower()
+        like_coffee = input("Do you like coffee?\n").lower()
         
-        elif like_tea == "NO" or like_tea == "N":
-            print("I am sorry. That's all I have for now.")
+        if like_coffee == "yes" or like_coffee == "y":
+            print("That's great! I like coffee too!")
         
+        elif like_coffee == "no" or like_coffee == "n":
+            print("You're missing out! Why not give it a try?")
+
+            like_tea = input("Do you like tea instead\n").upper()
+            if like_tea == "YES" or like_tea == "Y":
+                print("Good for you. Give coffee another try. :)")
+            
+            elif like_tea == "NO" or like_tea == "N":
+                print("I am sorry. That's all I have for now.")
+            
+            else:
+                print("I don't understand. Please answer with either a 'Yes' or a 'No'.")
+
         else:
             print("I don't understand. Please answer with either a 'Yes' or a 'No'.")
 
-    else:
-        print("I don't understand. Please answer with either a 'Yes' or a 'No'.")
+        keep_going = input("Press <ENTER> to continue, or any other key to quit. Thanks!\n")
 
-    keep_going = input("Press <ENTER> to continue, or any other key to quit. Thanks!\n")
+if __name__ == "__main__":
+    coffee_program()
